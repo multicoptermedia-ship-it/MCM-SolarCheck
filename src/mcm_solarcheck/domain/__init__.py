503ece33ae2865a3513a691f620eee5b9a301b01
multicoptermedia-ship-it <multicopter.media@gmail.com>
2026-09-11 13:32:16 +0200

    Export MCM domain models

---
+++ src/mcm_solarcheck/domain/__init__.py
@@ -0,0 +1,12 @@
+"""Vendor-neutral domain types."""
+
+from .models import ImageFrame, ImagePair, Position, Pose, RTKQuality, ThermalFrame
+
+__all__ = [
+    "ImageFrame",
+    "ImagePair",
+    "Position",
+    "Pose",
+    "RTKQuality",
+    "ThermalFrame",
+]
