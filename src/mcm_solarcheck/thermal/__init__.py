45ff9e9f0e859b02269509126bde05c0155547f1
multicoptermedia-ship-it <multicopter.media@gmail.com>
2026-09-15 11:41:49 +0200

    Add thermal parser package

---
+++ src/mcm_solarcheck/thermal/__init__.py
@@ -0,0 +1,5 @@
+"""Thermal image parsing and radiometric provider interfaces."""
+
+from .m3t_radiometric import M3TRadiometricParser, RadiometricRaster
+
+__all__ = ["M3TRadiometricParser", "RadiometricRaster"]
